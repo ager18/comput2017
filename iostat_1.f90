c604484911b2ad824e709b5f5bf993d140d470d1
ager_18 <ager_18@hotmail.com>
2017-07-12 19:13:16 -0500

    ejemplo para utilizar iostat leyendo desde el archivo io1.data

---
+++ iostat_1.f90
@@ -0,0 +1,20 @@
+INTEGER :: io
+REAL::x,y,sum
+sum = 0
+OPEN(1,FILE='io1.data',status="unknown")
+DO
+READ(1,*,IOSTAT=io) x,y
+IF (io > 0) THEN
+WRITE(*,*)'Check input Something &
+was wrong'
+EXIT
+ELSE IF (io < 0) THEN
+!para que entre aqui das ctrl-d
+WRITE(*,*) 'The total is ', sum
+EXIT
+ELSE
+sum = sum + y
+END IF
+END DO
+
+END PROGRAM
